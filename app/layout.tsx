a0a711a5b6a75dfbb84d298001f45a84915aadb7
David Broome <dsbroomeco@gmail.com>
2024-09-08 22:37:53 -0400

    :tada: ch16 add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+//autogenerate metadata for the project
+// the %s will be replaced by the title, if present, in other page.tsx's
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+}
 
 export default function RootLayout({
   children,
